2f97cbf02eec0f940f6384933485cc82d2e6b31f
Grazziano <grazzianofagundes@gmail.com>
2020-11-26 21:52:52 -0300

    Creating the login form

--- register.php
+++ register.php
@@ -13,6 +13,14 @@ require 'includes/form_handlers/register_handler.php';
 
 <body>
     <form action="register.php" method="post">
+        <input type="email" name="log_email" placeholder="Email Address">
+        <br>
+        <input type="password" name="log_password" placeholder="Password">
+        <br>
+        <input type="submit" name="login_button" value="Login">
+    </form>
+
+    <form action="register.php" method="post">
         <input type="text" name="reg_fname" placeholder="First Name" value="<?php if(isset($_SESSION['reg_fname'])){ echo $_SESSION['reg_fname']; } ?>" required>
         <br>
         <?php if (in_array("Your first name must be between 2 and 25 characters", $error_array)) echo "Your first name must be between 2 and 25 characters<br>"; ?>
