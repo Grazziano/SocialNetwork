83ad952f44f6e7bc3889212b21932834d8ecaac8
Grazziano <grazzianofagundes@gmail.com>
2020-11-28 22:49:57 -0300

    Adding bootstrap

--- includes/header.php
+++ includes/header.php
@@ -14,6 +14,9 @@ if (isset($_SESSION['username'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Welcome to SwirlFeed!</title>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    <script src="assets/js/bootstrap.js"></script>
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
 </head>
 
 <body>
