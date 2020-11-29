5b36f18a15dba16129778ca49f201b2d166a7639
Grazziano <grazzianofagundes@gmail.com>
2020-11-28 22:39:52 -0300

    Header page and redirecting users not logged in

--- index.php
+++ index.php
@@ -1,16 +1,6 @@
 <?php
-require 'config/config.php';
+include("includes/header.php");
 ?>
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome to SwirlFeed!</title>
-</head>
-
-<body>
 
 </body>
 
