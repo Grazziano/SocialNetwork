749641126e65fdb80e3d1553a277c5b28dea442c
Grazziano <grazzianofagundes@gmail.com>
2020-11-28 22:59:29 -0300

    Creating the blue top bar

--- includes/header.php
+++ includes/header.php
@@ -14,9 +14,20 @@ if (isset($_SESSION['username'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Welcome to SwirlFeed!</title>
+
+    <!-- Javascript -->
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <script src="assets/js/bootstrap.js"></script>
+
+    <!-- CSS -->
     <link rel="stylesheet" href="assets/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+
 </head>
 
 <body>
+    <div class="top_bar">
+        <div class="logo">
+            <a href="index.php">SwirlFeed</a>
+        </div>
+    </div>
