4e27afd8ec0d83e8b276c7a5b1f063142c4cbca7
Grazziano <grazzianofagundes@gmail.com>
2020-12-22 21:50:02 -0300

    Add friend button

--- includes/classes/User.php
+++ includes/classes/User.php
@@ -63,4 +63,28 @@ class User
             return false;
         }
     }
+
+    public function didReceiveRequest($user_to)
+    {
+        $user_from = $this->user['username'];
+        $check_request_query = mysqli_query($this->con, "SELECT * FROM friend_request WHERE user_to = '$user_to' AND user_from = '$user_from'");
+
+        if (mysqli_num_rows($check_request_query) > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function didSendRequest($user_from)
+    {
+        $user_to = $this->user['username'];
+        $check_request_query = mysqli_query($this->con, "SELECT * FROM friend_request WHERE user_to = '$user_to' AND user_from = '$user_from'");
+
+        if (mysqli_num_rows($check_request_query) > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
