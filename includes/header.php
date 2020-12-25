9b896a9b18ca1d48da88f0ce783d428247a33535
Grazziano <grazzianofagundes@gmail.com>
2020-12-24 23:59:54 -0300

    Creating our main javascript file

--- includes/header.php
+++ includes/header.php
@@ -22,6 +22,7 @@ if (isset($_SESSION['username'])) {
     <!-- Javascript -->
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <script src="assets/js/bootstrap.js"></script>
+    <script src="assets/js/swirlFeed.js"></script>
 
     <!-- CSS -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
