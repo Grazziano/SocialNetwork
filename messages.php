19fb1596c8cfdc7f961aad5c2fb8fff4b8f1b21d
Grazziano <grazzianofagundes@gmail.com>
2020-12-27 00:25:42 -0300

    Messages page heading

--- messages.php
+++ messages.php
@@ -1,7 +1,7 @@
 <?php
 include("includes/header.php");
 
-$message = new Message($con, $userLoggedIn);
+$message_obj = new Message($con, $userLoggedIn);
 
 if (isset($_GET['u'])) {
     $user_to = $_GET['u'];
@@ -11,3 +11,33 @@ if (isset($_GET['u'])) {
         $user_to = 'new';
     }
 }
+
+if ($user_to != "new") {
+    $user_to_obj = new User($con, $user_to);
+}
+?>
+
+<div class="user_details column">
+    <a href="<?php echo $userLoggedIn ?>"><img src="<?php echo $user['profile_pic']; ?>" alt=""></a>
+
+    <div class="user_details_left_right">
+        <a href="<?php echo $userLoggedIn ?>">
+            <?php
+            echo $user['first_name'] . " " . $user['last_name'];
+            ?>
+        </a>
+        <br>
+        <?php
+        echo "Posts: " . $user['num_posts'] . "<br>";
+        echo "Likes " . $user['num_likes'];
+        ?>
+    </div>
+</div>
+
+<div class="main_column column" id="main_column">
+    <?php
+    if ($user_to != "new") {
+        echo "<h4>You and <a href='$user_to'>" . $user_to_obj->getFirstAndLastName() . "</a></h4>";
+    }
+    ?>
+</div>
