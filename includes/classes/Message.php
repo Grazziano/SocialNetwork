d2f575271bed9d9a9cf4390582638a24aefc3ba9
Grazziano <grazzianofagundes@gmail.com>
2020-12-31 20:39:04 -0300

    Getting the conversations list - Finishing Off

--- includes/classes/Message.php
+++ includes/classes/Message.php
@@ -64,7 +64,7 @@ class Message
     {
         $details_array = array();
 
-        $query = mysqli_query($this->con, "SELECT body, user_to FROM messages WHERE (user_to = '$userLoggedIn' AND user_from = '$user2') 
+        $query = mysqli_query($this->con, "SELECT body, user_to, date FROM messages WHERE (user_to = '$userLoggedIn' AND user_from = '$user2') 
                                             OR (user_to = '$user2' AND user_from = '$userLoggedIn') ORDER BY id DESC LIMIT 1");
 
         $row = mysqli_fetch_array($query);
@@ -72,7 +72,7 @@ class Message
 
         //Timeframe
         $date_time_now = date("Y-m-d H:i:s");
-        $start_date = new DateTime($date_time); //Time of post
+        $start_date = new DateTime($row['date']); //Time of post
         $end_date = new DateTime($date_time_now); //Current time
         $interval = $start_date->diff($end_date); //Difference between dates 
         if ($interval->y >= 1) {
@@ -133,7 +133,7 @@ class Message
         $return_string = "";
         $convos = array();
 
-        $query = mysqli_query($this->con, "SELECT user_to, user_from FROM messages WHERE user_to = '$userLoggedIn' OR user_from = '$userLoggedIn'");
+        $query = mysqli_query($this->con, "SELECT user_to, user_from FROM messages WHERE user_to = '$userLoggedIn' OR user_from = '$userLoggedIn' ORDER BY id DESC");
 
         while ($row = mysqli_fetch_array($query)) {
             $user_to_push = ($row['user_to'] != $userLoggedIn) ? $row['user_to'] : $row['user_from'];
@@ -150,6 +150,16 @@ class Message
             $dots = (strlen($latest_message_details[1]) >= 12) ? "..." : "";
             $split = str_split($latest_message_details[1], 12);
             $split = $split[0] . $dots;
+
+            $return_string .= "<a href='messages.php?u=$username'>
+                                    <div class='user_found_messages'>
+                                        <img src='" . $user_found_obj->getProfilePic() . "' style='border-radius: 5px; margin-right: 5px;'>
+                                        " . $user_found_obj->getFirstAndLastName() . "
+                                        <span class='timestamp' id='grey'>" . $latest_message_details[2] . "</span>
+                                        <p id='grey' style='margin: 0;'>" . $latest_message_details[0] . $split . "</p>
+                                    </div>
+                                </a>";
         }
+        return $return_string;
     }
 }
