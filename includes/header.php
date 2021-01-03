7410383d2200cc4b1092a321da5cd4725d819c6f
Grazziano <grazzianofagundes@gmail.com>
2021-01-02 22:20:10 -0300

    Infinite scrolling - Part 1

--- includes/header.php
+++ includes/header.php
@@ -56,4 +56,46 @@ if (isset($_SESSION['username'])) {
         </div>
     </div>
 
+    <script>
+        var userLoggedIn = '<?php echo $userLoggedIn; ?>';
+
+        $(document).ready(function() {
+
+            $(window).scroll(function() {
+                var inner_height = $('.dropdown_data_window').innerHeight(); //Div containing data
+                var scroll_top = $('.dropdown_data_window').scrollTop();
+                var page = $('.dropdown_data_window').find('.nextPageDropdownData').val();
+                var noMoreData = $('.dropdown_data_window').find('.noMoreDropdownData').val();
+
+                if ((scroll_top + inner_height >= $('.dropdown_data_window')[0].scrollHeight) && noMoreData == 'false') {
+
+                    var pageName; // Holds name of page to send ajax request to
+                    var type = $('#dropdown_data_type').val();
+
+                    if (type == 'notification') {
+                        pageName = "ajax_load_notification.php";
+                    } else if (type == 'message') {
+                        pageName = "ajax_load_messages.php";
+                    }
+
+                    var ajaxReq = $.ajax({
+                        url: "includes/handlers/ajax_load_posts.php",
+                        type: "POST",
+                        data: "page=" + page + "&userLoggedIn=" + userLoggedIn,
+                        cache: false,
+
+                        success: function(response) {
+                            $('.posts_area').find('.nextPage').remove(); //Removes current .nextpage 
+                            $('.posts_area').find('.noMorePosts').remove(); //Removes current .nextpage 
+
+                            $('#loading').hide();
+                            $('.posts_area').append(response);
+                        }
+                    });
+                } //End if 
+                return false;
+            }); //End (window).scroll(function())
+        });
+    </script>
+
     <div class="wrapper">
