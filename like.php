fd4e0104f3b5f84b178cea752c506ac3489ac65c
Grazziano <grazzianofagundes@gmail.com>
2021-01-17 18:29:35 -0300

    Liked post notification

--- like.php
+++ like.php
@@ -61,6 +61,10 @@
         $insert_user = mysqli_query($con, "INSERT INTO likes VALUES('', '$userLoggedIn', '$post_id')");
 
         # Insert Notification
+        if ($user_liked != $userLoggedIn) {
+            $notification = new Notification($this->con, $userLoggedIn);
+            $notification->insertNotification($post_id, $user_to, "like");
+        }
     }
 
     # Unlike button
