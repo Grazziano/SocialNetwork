0ad2118e2a4d2dfe93e6ddc053674163a1c0225a
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2021-04-28 12:17:00 -0300

    Configuring the return string

--- includes/handlers/ajax_search.php
+++ includes/handlers/ajax_search.php
@@ -15,3 +15,28 @@ else if (count($names) == 2)
 // If query has one word only, search first names or last names
 else
     $usersReturnedQuery = mysqli_query($con, "SELECT * FROM users WHERE (first_name LIKE '$names[0]%' AND last_name LIKE '$names[0]%') AND user_closed = 'no' LIMIT 8");
+
+if ($query != "") {
+    while ($row = mysqli_fetch_array($usersReturnedQuery)) {
+        $user = new User($con, $userLoggedIn);
+
+        if ($row['username'] != $userLoggedIn) {
+            $mutual_friends = $user->getMutualFriends($row['username']) . " friends in common";
+        } else {
+            $mutual_friends = "";
+        }
+        echo "<div class='resultDisplay'>
+                <a href='" . $row['username'] . "' style='color: #1485BD'>
+                    <div class='liveSearchProfilePic'>
+                        <img src='" . $row['profile_pic'] . "'>
+                    </div>
+
+                    <div class='liveSearchText'>
+                        " . $row['first_name'] . " " . $row['last_name'] . "
+                        <p>" . $row['username'] . "</p>
+                        <p ='grey'>" . $mutual_friends . "</p>
+                    </div>
+                </a>
+            </div>";
+    }
+}
