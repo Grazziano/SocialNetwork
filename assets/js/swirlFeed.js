68a0f8d2f3d916207ba0bc5bf0dbd1ca34380e03
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2021-04-28 16:51:43 -0300

    Hiding the live Search results when clicking away

--- assets/js/swirlFeed.js
+++ assets/js/swirlFeed.js
@@ -30,6 +30,20 @@ $(document).ready(function () {
 
 });
 
+$(document).click(function (e) {
+    if (e.target.class != "search_results" && e.target.id != "search_text_input") {
+        $(".search_results").html("");
+        $('.search_results_footer').html("");
+        $('.search_results_footer').toggleClass("search_results_footer_empty");
+        $('.search_results_footer').toggleClass("search_results_footer");
+    }
+
+    if (e.target.class != "dropdown_data_window") {
+        $(".dropdown_data_window").html("");
+        $(".dropdown_data_window").css({ "padding": "0px", "height": "0px" });
+    }
+})
+
 function getUsers(value, user) {
     $.post("includes/handlers/ajax_friend_search.php", { query: value, userLoggedIn: user }, function (data) {
         $(".results").html(data);
